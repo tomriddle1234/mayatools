c1a34b36ccae5da939b725936f47845c5718aa9a
tomriddle1234 <tomriddle_1234@hotmail.com>
2015-03-05 17:23:01 +0800

    Added load model function, GUI is done.

--- simpleModelPreviewer.py
+++ simpleModelPreviewer.py
@@ -1,60 +1,191 @@
 import maya.cmds as cmds
 import maya.OpenMaya as om
 
-class createMyLayoutCls(object):
+
+class simpleModelPreviewer(object):
     def __init__(self, *args):
-        pass
+        self._name = "simpleModelPreviewer"
+        self._title = "Simple Model Previewer"
+
+        self._modelFilePath = ""
+        self._renderOutputFilePath = ""
+
+        self.meshList = []
+
     def show(self):
-        self.createMyLayout()
-    def createMyLayout(self):
-        self.window = cmds.window(widthHeight=(400, 200), title="Simple Model Previewer",   resizeToFitChildren=1)
-        cmds.rowLayout("button1, button2, button3", numberOfColumns=5)
+        self.createLayout()
 
-        cmds.columnLayout(adjustableColumn=True, columnAlign="center", rowSpacing=10)
+    def createLayout(self):
+        # create UI
+        win = self._name
+        if cmds.window(win, ex=True):
+            cmds.deleteUI(win)
 
-        self.button1 = cmds.textFieldButtonGrp(label="LocatorCurve",
-                                        text="Please key in your coordinates",
-                                        changeCommand=self.edit_curve,
-                                        buttonLabel="Execute",
-                                        buttonCommand=self.locator_curve)
-        
-        self.button2 = cmds.textFieldButtonGrp(label="LoadModel",
+        win = cmds.window(self._name, widthHeight=(400, 200), title=self._title, resizeToFitChildren=1)
+
+        cmds.columnLayout(adjustableColumn=True, columnAlign="center", rowSpacing=5)
+
+        cmds.separator()
+        cmds.text("Load Model File")
+        cmds.separator()
+
+        cmds.columnLayout(adjustableColumn=True, columnAlign="left", rowSpacing=5)
+
+        self.button1 = cmds.textFieldButtonGrp(label="File Path:",
                                         text="Please select your model file",
-                                        buttonLabel="Execute",
-                                        buttonCommand=self.load_model)         
+                                        buttonLabel="Load Model",
+                                        width=200,
+                                        buttonCommand=self.load_model)
 
-        self.button3 = cmds.textFieldButtonGrp(label="Create Camera",
-                                        text="Click to create a render camera for selected model",
-                                        buttonLabel="Execute",
-                                        buttonCommand=self.load_model)                                       
+        self.button2 = cmds.button("Reload", enable=False, c=self.reload_model)
+
+        cmds.setParent("..")
+        cmds.separator()
+        cmds.text("Camera Options")
+        cmds.separator()
+        cmds.rowColumnLayout(numberOfColumns=2,
+                             columnWidth=[(1, 200), (2, 200)],
+                             cal=[(1, "right"), (2, "center")],
+                             cs=[(1, 5), (2, 5)],
+                             rs=[(1, 5), (2, 5)])
+        self.button3 = cmds.button("Create Camera", c=self.create_camera)
+        self.button4 = cmds.button("Adjust Camera", c=self.adjust_camera)
+        cmds.text("Animation Length (seconds):")
+        self.frameLength = cmds.floatField(minValue=0.0)
+        cmds.text("Animation FPS:")
+        self.animationFPS = cmds.optionMenu()
+        cmds.menuItem(label="12")
+        cmds.menuItem(label="24")
+        cmds.menuItem(label="25")
+        cmds.menuItem(label="30")
+        cmds.menuItem(label="50")
+        cmds.menuItem(label="60")
+
+        cmds.setParent("..")
+        self.button5 = cmds.button("Create Camera Motion", width=100, c=self.create_camera_motion)
+
+        cmds.separator()
+        cmds.text("Material Options")
+        cmds.separator()
+        cmds.rowColumnLayout(numberOfColumns=2,
+                             columnWidth=[(1, 200), (2, 200)],
+                             cal=[(1, "right"), (2, "center")],
+                             cs=[(1, 5), (2, 5)],
+                             rs=[(1, 5), (2, 5)])
+
+        self.materialOption = cmds.optionMenu()
+        cmds.menuItem(label="Stone")
+        cmds.menuItem(label="Plastic")
+        cmds.menuItem(label="Glass")
+        cmds.menuItem(label="Wood")
+        self.button6 = cmds.button("Assign Material", c=self.config_model_material)
+
+        cmds.setParent("..")
+        cmds.separator()
+        cmds.text("Output Options")
+        cmds.separator()
+
+        cmds.columnLayout(adjustableColumn=True, columnAlign="left", rowSpacing=5)
+        self.button7 = cmds.textFieldButtonGrp(label="Output File Path:",
+                                               text="Please select your output path.",
+                                               buttonLabel="Select Path",
+                                               width=200,
+                                               buttonCommand=self.config_output_path)
+
+        cmds.rowColumnLayout(numberOfColumns=2,
+                             columnWidth=[(1, 200), (2, 200)],
+                             cal=[(1, "right"), (2, "center")],
+                             cs=[(1, 5), (2, 5)],
+                             rs=[(1, 5), (2, 5)])
+
+        cmds.text("Choose Output Format")
+        self.outputFormatOption = cmds.optionMenu()
+        cmds.menuItem(label="png")
+        cmds.menuItem(label="tif")
+        cmds.menuItem(label="openEXR")
+
+        cmds.text("Start Frame:")
+        self.startFrame = cmds.intField(minValue=0)
+        cmds.text("End Frame:")
+        self.endFrame = cmds.intField(minValue=0)
+
+        cmds.text("Output Width:")
+        self.outputWidth = cmds.intField(minValue=0)
+        cmds.text("OutputHeight:")
+        self.outputHeight = cmds.intField(minValue=0)
+
+        cmds.setParent("..")
+        cmds.separator()
+        self.renderButton = cmds.button("RENDER", width=200, backgroundColor=[0, 1, 0], c=self.renderOutput)
+        cmds.separator()
+
+
         cmds.setParent(menu=True)
 
-        cmds.showWindow(self.window)
+        cmds.showWindow(win)
 
-    def locator_curve(self,*args):
-        # Coordinates of the locator-shaped curve.
-        crv = cmds.curve(degree=1,
-                     point=[(1, 0, 0),
-                            (-1, 0, 0),
-                            (0, 0, 0),
-                            (0, 1, 0),
-                            (0, -1, 0),
-                            (0, 0, 0),
-                            (0, 0, 1),
-                            (0, 0, -1),
-                            (0, 0, 0)])
-        return crv
 
-    def edit_curve(self,*args):
-        parts = self.button1.split(",")
-        print parts
-        txt_val = cmds.textFieldButtonGrp(self.button1, q=True,text=True)
-        print txt_val
-        
+
     def load_model(self, *args):
+        filter = "All Files (*.*);;Maya Ascii (*.ma);;Maya Binary (*.mb)"
+        self.load_model_dialog = cmds.fileDialog2(fileFilter=filter, dialogStyle=1, fm=1)
+
+        if self.load_model_dialog is None:
+            return
+        if not (len(self.load_model_dialog)):
+            return
+
+        self.inputModelFilename = self.load_model_dialog[0]
+        print self.inputModelFilename
+        if self.inputModelFilename != "":
+            cmds.textFieldButtonGrp(self.button1, text=self.inputModelFilename, e=True)
+            cmds.button(self.button2, e=True, enable=True)
+            self.reload_model(self.inputModelFilename)
+        else:
+            print ("Error, model file name is empty.")
+
+    def reload_model(self, *args):
+        # check self.inputModelFilename
+        if self.inputModelFilename:
+            #check if scene is empty or show a dialog
+            currentMeshList = cmds.ls(type='mesh')
+            if len(currentMeshList) > 0:
+                print ("Warning, current scene has other mesh objects:")
+                print (str(currentMeshList))
+
+                #user should remove all the mesh objects, otherwise they may get unwanted result.
+                val = cmds.confirmDialog(title='Scene is not empty.',
+                                         message='Please prepare an empty scene to better use this plugin. ',
+                                         button=['OK', 'Ignore'])
+
+                #press OK to prepare the scene manually.
+                if val == "OK":
+                    cmds.textFieldButtonGrp(self.button1, text="Please select your model file", e=True)
+                    cmds.button(self.button2, e=True, enable=False)
+                    return
+
+            print ("Loading %s..." % self.inputModelFilename)
+            try:
+                cmds.file(self.inputModelFilename, i=True)
+            except:
+                print ("Error, unable to load file %s." % self.inputModelFilename)
+                return
+
+            #record the mesh objects
+            self.meshList = cmds.ls(type='mesh')
+
+        else:
+            print ("Error, input model filename is empty.")
+
+
+
+
+
         pass
-        
     def create_camera(self, *args):
+        pass
+
+    def adjust_camera(self):
         pass
     
     def create_camera_motion(self, *args):
@@ -81,5 +212,9 @@ class createMyLayoutCls(object):
     def config_output_resolution(self, optionIdx):
         pass
 
-b_cls = createMyLayoutCls()  
+    def renderOutput(self):
+        pass
+
+
+b_cls = simpleModelPreviewer()
 b_cls.show()
